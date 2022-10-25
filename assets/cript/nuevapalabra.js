73b71216921d6bcd168ce61877028056643b22ef
Mauro Andrade <mauritomza@hotmail.com>
2022-10-25 10:42:55 -0300

    Challenge terminado

--- assets/cript/nuevapalabra.js
+++ assets/cript/nuevapalabra.js
@@ -1,3 +1,4 @@
+const mainJuego = document.querySelector(".main__juego");
 const formulario = document.querySelector("[data-form]");
 const botonPalabra = document.querySelector(".nuevaPalabra")
 const botonJuego = document.querySelector(".comenzar");
@@ -40,6 +41,10 @@ botonJuego.addEventListener("click", (evento) => {
     const palabraAleatoria = palabraStorage[Math.floor(Math.random() * palabraStorage.length)];
     const palabraExtra = palabraAleatoria.nuevaPalabra;
     const palabraArray = Array.from(palabraExtra);
+    const imagen = document.querySelector(".imagenHorca");
+    let aciertos = 0;
+    let vidas = 0;
+
 
     for (var i = 0; i < palabraArray.length; i++) {
         const crearGuiones = document.createElement("p");
@@ -50,17 +55,52 @@ botonJuego.addEventListener("click", (evento) => {
 
     document.addEventListener("keyup", (events) => {
         const letra = events.key;
-        const vidas = palabraArray.length;
-        const guion = document.querySelector(".incorrecta");
-        palabraArray.forEach(element => {
-            if (letra == element) {
-                guion.textContent = letra;
+        const guiones = document.querySelectorAll(".incorrecta");
+        let palabraExiste = false;
+
+        for (var i = 0; i < palabraArray.length; i++) {
+            letraPalabra = palabraArray[i];
+            if (letra == letraPalabra) {
+                guiones[i].innerHTML = letra;
+                palabraExiste = true;
+                aciertos = aciertos + 1;
             }
-            else {
-                console.log("perdiste un turno");
-            }
-        });
+        }
+
+        if (aciertos == palabraArray.length) {
+            const botonIniciarNuevo = document.createElement("button");
+            botonIniciarNuevo.textContent = "volver a jugar";
+            juegoPalabra.appendChild(botonIniciarNuevo);
+            botonIniciarNuevo.addEventListener("click", () => {
+                volverForm();
+            });
+        }
+        else if (vidas == 6) {
+            alert("perdiste, la palabra era: " + palabraArray.join(''));
+            volverForm();
+        }
+
+        if (!palabraExiste) {
+            vidas = vidas + 1;
+            imagen.setAttribute("src", `assets/imagenes/horca${vidas}.png`);
+        }
+
     })
 
 })
+
+function volverForm() {
+    juegoPalabra.remove();
+    const elementoDiv = document.createElement("div");
+    elementoDiv.classList.add("juego__palabra");
+    elementoDiv.innerHTML = `<form class="ingresaPalabra" data-form>
+    <label for="palabraNueva">Ingresa una nueva Palabra</label>
+    <input type="text" name="palabraNueva" id="palabraNueva">
+    <p data-error></p>
+    <button class="nuevaPalabra">Agregar Palabra </button>
+    <button class="comenzar">Comenzar juego</button>
+</form>`;
+
+    mainJuego.appendChild(elementoDiv);
+}
 
